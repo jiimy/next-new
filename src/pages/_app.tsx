694389a3de37d0eb6b4fd58de8e2878bf59efe91
rudwnok <rudwnok123@naver.com>
2023-06-21 19:56:33 +0900

    배포 테스트

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+  Hydrate
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, ...rest }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 0,
+        suspense: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  }));
+  return <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <Component {...pageProps} />
+    </Hydrate>
+  </QueryClientProvider>
 }
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from "";
+
+const Main = () => {
+  async function getBoardList() {
+    const { data } = await axios.get(`https://64927c19428c3d2035d02a9d.mockapi.io/api/todo`);
+    return data;
+  }
+
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ['getTodo'],
+    queryFn: getBoardList,
+  });
+
+  console.log('dd', data);
+
+  return (
+    <div>
+      메인
+      <button>post</button>
+    </div>
+  )
+}
+
+export default Main
