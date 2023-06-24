0f3884d06808a35425656829b84e1c8b3311269a
rudwnok <rudwnok123@naver.com>
2023-06-24 11:00:50 +0900

    상세페이지 isr 제거 배포 테스트

--- src/pages/todo/[id].tsx
+++ src/pages/todo/[id].tsx
@@ -13,30 +13,30 @@ interface PostProps {
 
 
 
-export async function getStaticPaths() {
-  const { data: posts } = await axios.get(`${ROOT_API}/todos`);
+// export async function getStaticPaths() {
+//   const { data: posts } = await axios.get(`${ROOT_API}/todos`);
 
-  // 모든 글의 ID로 경로를 생성합니다.
-  const paths = posts.map((post: any) => ({
-    params: { id: post.id.toString() },
-  }));
+//   // 모든 글의 ID로 경로를 생성합니다.
+//   const paths = posts.map((post: any) => ({
+//     params: { id: post.id.toString() },
+//   }));
 
-  return {
-    paths,
-    fallback: true, // fallback을 true로 설정하여 없는 경로로의 접근 시 404 페이지를 자동으로 생성하도록 합니다.
-  };
-}
+//   return {
+//     paths,
+//     fallback: true, // fallback을 true로 설정하여 없는 경로로의 접근 시 404 페이지를 자동으로 생성하도록 합니다.
+//   };
+// }
 
 
-export async function getStaticProps({ params }: any) {
-  const { data: post } = await axios.get(`${ROOT_API}/todos/${params.id}`);
-  return {
-    props: {
-      post
-    },
-    revalidate: 30,
-  };
-}
+// export async function getStaticProps({ params }: any) {
+//   const { data: post } = await axios.get(`${ROOT_API}/todos/${params.id}`);
+//   return {
+//     props: {
+//       post
+//     },
+//     revalidate: 30,
+//   };
+// }
 
 
 const Index = () => {
