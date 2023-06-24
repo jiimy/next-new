9b89463e69bafbb55247234a3d280cbfa5f22e16
rudwnok <rudwnok123@naver.com>
2023-06-24 18:21:08 +0900

    isr 제거

--- src/pages/isr.tsx
+++ src/pages/isr.tsx
@@ -1,42 +1,42 @@
-import { ROOT_API } from '@/constants/api';
-import { useQuery, QueryClient, dehydrate } from '@tanstack/react-query';
-import axios from 'axios';
+// import { ROOT_API } from '@/constants/api';
+// import { useQuery, QueryClient, dehydrate } from '@tanstack/react-query';
+// import axios from 'axios';
 
 
-type SsgType = {
-  data?: any
-}
+// type SsgType = {
+//   data?: any
+// }
 
-const Isr = () => {
-  const { data } = useQuery(['todos'], getBoardList)
-  return (
-    <div>
-      <h1>isr 페이지</h1>
-      <div>
-        {data.map((item: any, i: any) => (
-          <div key={i}>{item.id} {item.title}</div>
-        ))}
-      </div>
-    </div>
-  )
-}
+// const Isr = () => {
+//   const { data } = useQuery(['todos'], getBoardList)
+//   return (
+//     <div>
+//       <h1>isr 페이지</h1>
+//       <div>
+//         {data.map((item: any, i: any) => (
+//           <div key={i}>{item.id} {item.title}</div>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
 
-export async function getBoardList() {
-  const { data } = await axios.get(`${ROOT_API}/todos`);
-  return data;
-}
+// export async function getBoardList() {
+//   const { data } = await axios.get(`${ROOT_API}/todos`);
+//   return data;
+// }
 
-export async function getStaticProps() {
-  const queryClient = new QueryClient();
+// export async function getStaticProps() {
+//   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery(['todos'], getBoardList);
+//   await queryClient.prefetchQuery(['todos'], getBoardList);
 
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-    revalidate: 30,
-  };
-}
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//     revalidate: 30,
+//   };
+// }
 
-export default Isr
+// export default Isr
