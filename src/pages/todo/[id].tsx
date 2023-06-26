1f5292d655cb5d06aa68b690c9a24803da2f3a22
rudwnok <rudwnok123@naver.com>
2023-06-26 19:09:20 +0900

    getStaticPaths 이거 없다면?

--- src/pages/todo/[id].tsx
+++ src/pages/todo/[id].tsx
@@ -11,19 +11,19 @@ interface TodoItemProps {
 
 
 
-export async function getStaticPaths() {
-  const { data: posts } = await axios.get(`${ROOT_API}/todos`);
+// export async function getStaticPaths() {
+//   const { data: posts } = await axios.get(`${ROOT_API}/todos`);
 
-  // 모든 글의 ID로 경로를 생성합니다.
-  const paths = posts.map((post: any) => ({
-    params: { id: post.id.toString() },
-  }));
+//   // 모든 글의 ID로 경로를 생성합니다.
+//   const paths = posts.map((post: any) => ({
+//     params: { id: post.id.toString() },
+//   }));
 
-  return {
-    paths,
-    fallback: true, // fallback을 true로 설정하여 없는 경로로의 접근 시 404 페이지를 자동으로 생성하도록 합니다.
-  };
-}
+//   return {
+//     paths,
+//     fallback: true, // fallback을 true로 설정하여 없는 경로로의 접근 시 404 페이지를 자동으로 생성하도록 합니다.
+//   };
+// }
 
 
 export async function getStaticProps({ params, paths }: any) {
