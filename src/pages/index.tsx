430b77566d770eac2c62222021ccbb0d91e1ef05
rudwnok <rudwnok123@naver.com>
2023-07-18 21:22:23 +0900

    폴더 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Main from './main'
+import Main from './todo'
+import Url from './url'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main/>
+      {/* <Main/> */}
+      <Url/>
     </>
   )
 }
